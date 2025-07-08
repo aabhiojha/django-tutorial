df2593047a68b85c046e99ec0352d0110dab5e97
Abhishek Ojha <aabhiojha08@outlook.com>
2025-07-08 08:26:29 -0700

    django-docs tutorial-2 class based views with generics: holy shit thats concise

--- snippet/views.py
+++ snippet/views.py
@@ -1,18 +1,11 @@
 from snippet.models import Snippet
 from snippet.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-    
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
